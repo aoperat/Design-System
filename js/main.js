0141c8b652988452a10020aa1a8fc802a25fe646
aoperat <54986046+aoperat@users.noreply.github.com>
2023-04-13 15:33:38 +0900

    add alert, badges, modals components

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.button');
-  
-    buttons.forEach(function(button) {
-      button.addEventListener('click', function() {
-        alert('Button clicked: ' + this.textContent);
-      });
+
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            console.log('Button clicked: ' + this.textContent);
+        });
     });
+});
+
+// Modals
+const openModalButton = document.getElementById('openModal');
+const closeModalButtons = document.querySelectorAll('#closeModal, #closeModalFooter');
+const modal = document.getElementById('myModal');
+
+openModalButton.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    modal.style.display = 'none';
   });
-  
+});
+
+window.addEventListener('click', event => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
